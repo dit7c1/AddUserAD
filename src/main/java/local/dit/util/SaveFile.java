e2f7c97180adc702b3a988cdd0403b560b141ce3
dit7c1 <dit7c1@gmail.com>
2021-03-09 21:42:29 +0300

    формирование строки через .append();

--- src/main/java/local/dit/util/SaveFile.java
+++ src/main/java/local/dit/util/SaveFile.java
@@ -8,20 +8,27 @@ import java.util.Date;
 
 public class SaveFile {
 
-    //todo сделать формирование строки через .append
     public void save(UserData userData) {
         String date = new SimpleDateFormat("dd.MM.yyyy").format(new Date());
         try (FileWriter writer = new FileWriter(userData.getUserLogin() + "." + date + ".txt", false)) {
-            writer.write(userData.getLastName() + " ");
-            writer.write(userData.getFirstNamePatronymic() + " ");
-            writer.write(userData.getPatronymic());
-            writer.write("\n");
-            writer.write(userData.getUserEmail());
-            writer.write("\n");
-            writer.write(userData.getUserPassword());
-            writer.write("\n");
-            writer.flush();
-            System.out.println("success save");
+            StringBuilder str = new StringBuilder();
+            str.append("Сотрудник: \t");
+            str.append(userData.getLastName());
+            str.append(" ");
+            str.append(userData.getFirstName());
+            str.append(" ");
+            str.append(userData.getPatronymic());
+            str.append("\n");
+            str.append("Логин: \t\t");
+            str.append(userData.getUserLogin());
+            str.append("\n");
+            str.append("E-mail: \t");
+            str.append(userData.getUserEmail());
+            str.append("\n");
+            str.append("Пароль: \t");
+            str.append(userData.getUserPassword());
+            writer.write(str.toString());
+            //writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
