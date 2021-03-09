a5348de9cdfa45f882955fe267be54eba309163b
dit7c1 <dit7c1@gmail.com>
2021-03-09 11:33:10 +0300

    main

---
+++ src/main/java/local/dit/App.java
@@ -0,0 +1,28 @@
+package local.dit;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("uiFX.fxml"));
+        stage.setTitle("Create User Active Directory");
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
